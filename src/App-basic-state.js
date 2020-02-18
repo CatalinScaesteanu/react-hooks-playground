633ca18e87d3d23196bceed8a84fe0ae9676639f
Catalin Scaesteanu <catalinscaesteanu@gmail.com>
2020-02-18 09:24:58 +0200

    useState

---
+++ src/App-basic-state.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(10);
+  const [count2, setCount2] = useState(20);
+  return (
+    <div>
+      <button onClick={() => {
+        setCount(c => c + 1);
+        setCount2(c => c + 1);
+      }}>+</button>
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div>
+    </div>
+  );
+};
+
+export default App;
